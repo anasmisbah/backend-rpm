0f35f872a23e8557412689356405e428e7574dbc
anasmisbah <anas.misbahuddin4869@gmail.com>
2020-02-26 01:16:01 +0800

    read post

--- resources/views/event/read.blade.php
+++ resources/views/event/read.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.post')
 @section('description')
+<h5 class="text-center text-bold">{{$event->title}}</h5>
+<img src="{{asset('/uploads/'.$event->image)}}" class="img-fluid" alt="">
 {!! $event->description !!}
 @endsection
--- resources/views/news/read.blade.php
+++ resources/views/news/read.blade.php
@@ -1,5 +1,7 @@
 
 @extends('layouts.post')
 @section('description')
+<h5 class="text-center text-bold">{{$news->title}}</h5>
+<img src="{{asset('/uploads/'.$news->image)}}" class="img-fluid" alt="">
 {!! $news->description !!}
 @endsection
