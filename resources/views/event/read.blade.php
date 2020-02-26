a3238da9a99ee199e3d4a4c985aaa44b19623690
anasmisbah <anas.misbahuddin4869@gmail.com>
2020-02-26 20:19:49 +0800

    simple change read post

--- resources/views/event/read.blade.php
+++ resources/views/event/read.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.post')
 @section('description')
-<h5 class="text-center text-bold">{{$event->title}}</h5>
+<p class="text-left  text-bold h5">{{$event->title}}</p>
+<hr style="border-top: 1px solid black">
+<p><i class="fa fa-clock"></i>{{" ".$event->created_at->format('d F Y')}}</p>
 <img src="{{asset('/uploads/'.$event->image)}}" class="img-fluid" alt="">
 {!! $event->description !!}
 @endsection
--- resources/views/news/read.blade.php
+++ resources/views/news/read.blade.php
@@ -1,7 +1,9 @@
 
 @extends('layouts.post')
 @section('description')
-<h5 class="text-center text-bold">{{$news->title}}</h5>
+<p class="text-left  text-bold h5">{{$news->title}}</p>
+<hr style="border-top: 1px solid black">
+<p><i class="fa fa-clock"></i>{{" ".$news->created_at->format('d F Y')}}</p>
 <img src="{{asset('/uploads/'.$news->image)}}" class="img-fluid" alt="">
 {!! $news->description !!}
 @endsection
